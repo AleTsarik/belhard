190d4475571ba6e40ca193ad45bafc48063f84ed
AleTsarik <alex.tsarik@gmail.com>
2023-02-15 23:04:27 +0300

    Lesson 1 Task 4

---
+++ Less_01_Task4.py
@@ -0,0 +1,13 @@
+# Пользователь вводит 3 числа, сказать сколько из них положительных
+# и сколько отрицательных
+
+# numb_1 = int(input('Введите первое число: '))
+# numb_2 = int(input('Введите второе число: '))
+# numb_3 = int(input('Введите третье число: '))
+
+numb_list = input('Введите 3 числа через пробел: ')
+
+print('Negative:')
+print(numb_list.count('-'))
+print('Positive:')
+print(3-(numb_list.count('-')))
