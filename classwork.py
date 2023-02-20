644a1b3ad1ba942ef9782e5bb1c248b00923dc2d
Student <kirillbazaff@gmail.com>
2023-02-20 17:15:19 +0300

    2

--- classwork.py
+++ classwork.py
@@ -1,3 +1,14 @@
 # Вводится число, найти максимальную цифру
-number = input('Entel value ')
-print(max(number))
+# number = input('Entel value ')
+# print(max(number))
+
+# Вводится сумма, написать минимально количество монет (25, 10, 5, 1) из которых ее можно составить
+
+# summ = int(input('Entel value '))
+# a = summ // 25
+# b = (summ - a*25) // 10
+# c = (summ - a*25 - b*10) // 5
+# d = (summ - a*25 - b*10 - c*5)
+#
+# print(a, b, c, d)
+# print(a+b+c+d)
