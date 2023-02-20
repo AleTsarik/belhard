c8c8f39def69fe90cb5c2e36336afa8308a226f3
AleTsarik <alex.tsarik@gmail.com>
2023-02-20 23:02:20 +0300

    Lesson 1 Task 4 v2

---
+++ hw1_4.py
@@ -0,0 +1,12 @@
+# пользователь вводит 3 числа, сказать сколько из них положительных
+# и сколько отрицательных
+
+numb_1 = (input('введите первое число: '))
+numb_2 = (input('введите второе число: '))
+numb_3 = (input('введите третье число: '))
+
+# print(numb_1.isdigit())
+# print(numb_2.isdigit())
+# print(numb_3.isdigit())
+
+print(numb_1.isdigit() + numb_2.isdigit() + numb_3.isdigit(), ' positive')
