1ee2f91780ffb55ab508ca18b4cf4bedf21f10a8
AleTsarik <alex.tsarik@gmail.com>
2023-02-20 22:34:22 +0300

    Lesson 2 Task 3 v1

---
+++ Lesson_02_Task3.py
@@ -0,0 +1,10 @@
+# **Вывести четные числа от 2 до N по 5 в строку
+n = int(input('Input N: '))
+i = 0
+for j in range(2, n+1):
+    if not j % 2:
+        print(j, end=' ')
+        i += 1
+        if i == 5:
+            print('')
+            i = 0
